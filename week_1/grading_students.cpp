82510bfa0e928935924a25ff6231bcc465637cb6
vickykimani <victorianightingale70@gmail.com>
2022-10-24 21:43:39 +0300

    grading students problem hackerrank

--- week_1/grading_students.cpp
+++ week_1/grading_students.cpp
@@ -5,7 +5,7 @@
 // Sam is a professor at the university and likes to round each student's  according to these rules:
 
 // If the difference between the  and the next multiple of  is less than , round  up to the next multiple of .
-// If the value of  is less than , no rounding occurs as the result will still be a failing grade.
+// If the value of  is less than, no rounding occurs as the result will still be a failing grade.
 
 #include <bits/stdc++.h>
 
