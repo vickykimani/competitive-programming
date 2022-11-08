ea81f0c3facdf8e82e881f403236bae094b8afc9
vickykimani <victorianightingale70@gmail.com>
2022-11-08 23:01:14 +0300

    merge intervals py

--- week_3/merge_intervals.py
+++ week_3/merge_intervals.py
@@ -1,3 +1,8 @@
+# Given an array of intervals where intervals[i] = [starti, endi], merge all
+# overlapping intervals, and return an array of the non-overlapping
+# intervals that cover all the intervals in the input.
+
+
 class Solution(object):
     def merge(self, intervals):
         out = []
