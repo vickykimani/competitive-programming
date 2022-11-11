dee90946dc7b201f53b5284d414e2ae2dae27f07
vickykimani <victorianightingale70@gmail.com>
2022-11-11 23:31:40 +0300

    kth largest int py

--- week_3/kth_largest_int.py
+++ week_3/kth_largest_int.py
@@ -1,3 +1,5 @@
+# use minHeap to keep the k largest elements so far, with
+# the kth largest element at the top of the heap.
 class Solution(object):
     def kthLargestNumber(self, nums, k):
         """
@@ -5,3 +7,12 @@ class Solution(object):
         :type k: int
         :rtype: str
         """
+
+        maxHeap = [-int(x) for x in nums]
+        heapify(maxHeap)
+
+        while k > 1:
+            heappop(maxHeap)
+            k -= 1
+
+        return str(-maxHeap[0])
